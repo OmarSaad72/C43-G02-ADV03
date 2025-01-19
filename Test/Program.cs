b8d0a73b09757903a20d27e52ad8a398584bd6f0
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-01-19 20:57:50 +0200

    HashTable{Non-Generic}

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Test
+﻿using System.Collections;
+
+namespace Test
 {
     internal class Program
     {
         public static void swap<T>(ref T x, ref T y)
         {
             T temp = x; x = y; y = temp;
+        }
+
+        class StringComparer : IEqualityComparer
+        {
+            public new bool Equals(object? x, object? y)
+            {
+                string? a = x as string;
+                string? b = y as string;
+                return a?.ToLower().Equals(b?.ToLower()) ?? (b is null ? true : false);
+            }
+
+            public int GetHashCode(object obj)
+            {
+                string? a = obj as string;
+                return a?.ToLower().GetHashCode() ?? 0;
+            }
         }
         static void Main()
         {
@@ -41,19 +59,38 @@
             //}
             #endregion
             #region Queue
-            Queue<int> queue = new Queue<int>();
-            queue.Enqueue(1); // add elements
-            queue.Enqueue(2);
-            queue.Enqueue(3);
-            queue.Dequeue(); // remove first element
-            queue.Dequeue(); // remove first element
-            queue.Dequeue(); // remove first element
-            queue.TryPeek(out int value);
-            //queue.TryDequeue(out int value2);
-            foreach (var i in queue)
-            {
-                Console.WriteLine(i);
-            }
+            //Queue<int> queue = new Queue<int>();
+            //queue.Enqueue(1); // add elements
+            //queue.Enqueue(2);
+            //queue.Enqueue(3);
+            //queue.Dequeue(); // remove first element
+            //queue.Dequeue(); // remove first element
+            //queue.Dequeue(); // remove first element
+            //queue.TryPeek(out int value);
+            ////queue.TryDequeue(out int value2);
+            //foreach (var i in queue)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+            #region HashTable{Non-Generic}
+            Hashtable note = new Hashtable(/*new StringComparer()*/);
+            note.Add("Omar", 7132);
+            note.Add("Mai", 2445);
+            //note.Add("mai", 445);  // Invalid
+            note.Add("Mohammed", 4332);
+            //Console.WriteLine(note["Omar"]);
+            //note["Mai"] = 123;
+            //Console.WriteLine(note["Mai"]);
+            //foreach (DictionaryEntry i in note)
+            //{
+            //    //Console.WriteLine(i);
+            //    Console.WriteLine($"{i.Key}: {i.Value}");
+            //}
+            Console.WriteLine(note.ContainsKey("Omar"));
+            Console.WriteLine(note.Contains("Sara"));
+            Console.WriteLine(note.ContainsValue(7132));
+            Console.WriteLine(note.ContainsValue(71));
             #endregion
         }
     }
