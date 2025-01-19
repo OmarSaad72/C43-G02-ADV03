c67356d10b2981bf066ddf97459d4c4aeeb75171
OmarSaad <OmarSaad@DESKTOP-2C0MESG>
2025-01-19 18:34:44 +0200

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        public static void swap<T>(ref T x, ref T y)
+        {
+            T temp = x; x = y; y = temp;
+        }
+        static void Main()
+        {
+            #region List
+
+            #endregion
+        }
+    }
+}
